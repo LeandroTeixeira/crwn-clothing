2e37726ed93c96a025227aa65de96346ff9f52a0
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-04-04 15:01:16 -0300

    Added category and category-item components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,28 @@
+import './category-item.style.scss';
+import React from 'react';
+import { number, string, shape } from 'prop-types';
+
+export default function CategoryItem({ category: { id, imageUrl, title } }) {
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
+
+CategoryItem.propTypes = {
+  category: shape({
+    id: number,
+    imageUrl: string,
+    title: string,
+  }),
+}.isRequired;
---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,18 @@
+import './category.style.scss';
+import React from 'react';
+import { arrayOf, object } from 'prop-types';
+import CategoryItem from '../category-item/category-item.component';
+
+export default function Category({ categories }) {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+Category.propTypes = {
+  categories: arrayOf(object),
+}.isRequired;
