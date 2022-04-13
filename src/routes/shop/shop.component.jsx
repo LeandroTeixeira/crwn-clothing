e13faffcdb6160bfa77cd6c013f4abd4f1199d03
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-04-13 14:48:21 -0300

    refactor: all stylesheets now have the styles extension in the title

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ShopContext } from '../../services/contexts';
 import ProductCard from '../../components/product-card/product-card.component';
-import './shop.style.scss';
+import './shop.styles.scss';
 
 export default function Shop() {
   const { products } = useContext(ShopContext);
