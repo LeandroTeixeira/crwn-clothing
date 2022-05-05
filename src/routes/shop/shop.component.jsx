d08dcb7cb1f6f3785f5d7de913fb9c16b65605e3
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-05-05 10:39:39 -0300

    feat: category-preview inside shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,23 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { ShopContext } from '../../services/contexts';
 import ProductCard from '../../components/product-card/product-card.component';
 import './shop.styles.scss';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 export default function Shop() {
   const { categoriesMap } = useContext(ShopContext);
-  const products = [];
-  Object.keys(categoriesMap).forEach(
-    (title) => products.push(...categoriesMap[title]),
-  );
   return (
-    <div className="products-container">
-      {products.map((product) => (<ProductCard key={product.id} product={product} />
-      ))}
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview
+            title={title}
+            products={products}
+            key={title}
+          />
+        );
+      })}
     </div>
   );
 }
